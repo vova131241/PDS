4d470369c54c5793df1da5d7ed98aa55d261c7ea
vova131241 <strashivskyivolodymyr@dspu.edu.ua>
2025-09-11 19:09:03 +0300

    Add files via upload

---
+++ Lab1_1Test.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab1_1/lab1_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab11Test
+{
+	TEST_CLASS(Lab11Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int q = 1;
+			int p = 0;
+			bool result = Conjuncted(q, p);
+			bool expectation = false;
+			Assert::AreEqual(result, expectation);
+		}
+	};
+}
---
+++ lab1_1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+void Calculate(int p, int q);
+bool Conjuncted(int p, int q);
+
+int main() {
+    bool p, q;
+    cout << "Choose value for p (0 or 1): ";
+    cin >> p;
+    cout << "Choose value for q (0 or 1): ";
+    cin >> q;
+
+    Calculate(p, q);
+
+    return 0;
+}
+
+void Calculate(int p, int q) {
+    bool conjunction = Conjuncted(p, q);
+    bool disjunction = p || q;
+    bool xorValue = p ^ q;
+    bool imp_pq = (!p) || q;
+    bool imp_qp = (!q) || p;
+    bool equivalence = ((!p) || q) && ((!q) || p);
+
+    cout << "p = " << p << ", q = " << q << endl;
+    cout << "conjunction (p AND q): " << conjunction << endl;
+    cout << "disjunction (p OR q): " << disjunction << endl;
+    cout << "xorValue (XOR): " << xorValue << endl;
+    cout << "imp_pq p → q: " << imp_pq << endl;
+    cout << "imp_qp q → p: " << imp_qp << endl;
+    cout << "equivalence (p ↔ q): " << equivalence << endl;
+}
+
+bool Conjuncted(int p, int q) {
+    return p && q;
+}
